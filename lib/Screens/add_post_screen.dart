718d293b99b319ae8c0d99d104de75a673fb8023
Rahul Gandhi <65856344+RGTechno@users.noreply.github.com>
2021-09-05 18:37:22 +0530

    navbar and project screens setup

---
+++ lib/Screens/add_post_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AddPostScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("Add Post Page"),
+      ),
+    );
+  }
+}
---
+++ lib/Screens/landing_screen.dart
@@ -0,0 +1,68 @@
+import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
+import "package:flutter/material.dart";
+import 'package:gdsc_usict/Screens/add_post_screen.dart';
+import 'package:gdsc_usict/Screens/messaging_screen.dart';
+import 'package:gdsc_usict/Screens/profile_screen.dart';
+
+import 'home_screen.dart';
+
+class LandingScreen extends StatefulWidget {
+  @override
+  _LandingScreenState createState() => _LandingScreenState();
+}
+
+class _LandingScreenState extends State<LandingScreen> {
+  PageController _pageController;
+  int pageIndex = 0;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    _pageController = PageController();
+    super.initState();
+  }
+
+  void _pageChangeFn(int pageIndex) {
+    setState(() {
+      this.pageIndex = pageIndex;
+    });
+  }
+
+  void _onTap(int pageIndex) {
+    setState(() {
+      _pageController.animateToPage(
+        pageIndex,
+        duration: Duration(milliseconds: 300),
+        curve: Curves.easeInOut,
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        children: [
+          HomeScreen(),
+          MessagingScreen(),
+          AddPostScreen(),
+          ProfileScreen(),
+        ],
+        controller: _pageController,
+        onPageChanged: _pageChangeFn,
+        physics: NeverScrollableScrollPhysics(),
+      ),
+      bottomNavigationBar: AnimatedBottomNavigationBar(
+        gapWidth: 2,
+        onTap: _onTap,
+        activeIndex: pageIndex,
+        icons: [
+          Icons.home,
+          Icons.notification_important_outlined,
+          Icons.add,
+          Icons.account_circle_outlined,
+        ],
+      ),
+    );
+  }
+}
