e83029df7ad1c4a8a75b0b19345e73f1af595721
aakzsh <aakashferrari@gmail.com>
2021-10-01 17:57:32 +0530

    add messaging layout

--- lib/Screens/messaging_screen.dart
+++ lib/Screens/messaging_screen.dart
@@ -1,22 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:gdsc_usict/Styles/messaging_styles.dart';
 import 'package:gdsc_usict/widgets/messaging_widget.dart';
 
 class MessagingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        resizeToAvoidBottomInset: true,
         body: Container(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
-            child: ListTile(
-                title: Text("GDSC USICT Chat"), trailing: CircleAvatar()),
+          color: Colors.blueAccent,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Padding(
+                  padding: EdgeInsets.fromLTRB(10, 30, 20, 10),
+                  child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        "GDSC USICT Community Forum",
+                        style: textStyle(Colors.white, 25.0, FontWeight.bold),
+                      ))),
+              Messages()
+            ],
           ),
-          Messages()
-        ],
-      ),
-    ));
+        ));
   }
 }
