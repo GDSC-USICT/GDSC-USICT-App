b2e4b1b322e0a7a79ce4569a383f5f83858bf444
aakzsh <aakashferrari@gmail.com>
2021-10-01 16:25:24 +0530

    add skeleton files for messaging

--- lib/Screens/messaging_screen.dart
+++ lib/Screens/messaging_screen.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:gdsc_usict/widgets/messaging_widget.dart';
 
 class MessagingScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Message Page"),
+        body: Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+            child: ListTile(
+                title: Text("GDSC USICT Chat"), trailing: CircleAvatar()),
+          ),
+          Messages()
+        ],
       ),
-    );
+    ));
   }
 }
