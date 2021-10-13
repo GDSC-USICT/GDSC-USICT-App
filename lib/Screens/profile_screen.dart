9747458b90e24a3b384b33de882914f2e12f366c
saurabhdhingra <75309640+saurabhdhingra@users.noreply.github.com>
2021-10-13 16:25:37 +0530

    Profile page layout

--- lib/Screens/profile_screen.dart
+++ lib/Screens/profile_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:gdsc_usict/constants.dart';
+import 'package:gdsc_usict/models/users.dart';
+import 'package:gdsc_usict/functions.dart';
+import 'package:gdsc_usict/models/posts.dart';
 
 class ProfileScreen extends StatefulWidget {
   @override
@@ -12,6 +15,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
   Widget build(BuildContext context) {
     var height = SizeConfig.getHeight(context);
     var width = SizeConfig.getWidth(context);
+    final user = UserPreferences.myUser;
 
     return Scaffold(
       backgroundColor: Color(0xFFF2F1F6),
@@ -51,7 +55,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
                     color: Colors.white,
                     icon: Icon(Icons.chevron_left),
                     onPressed: () {
-                      print('Button pressed');
+                      print('Back pressed');
                     },
                   ),
                 ),
@@ -63,7 +67,64 @@ class _ProfileScreenState extends State<ProfileScreen> {
                   height: height * 0.3,
                   decoration: BoxDecoration(
                     color: Colors.white,
-                    borderRadius: BorderRadius.all(Radius.circular(35)),
+                    borderRadius:
+                        BorderRadius.all(Radius.circular(height / 38)),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      SizedBox(
+                        height: height * 0.01,
+                      ),
+                      Container(
+                        height: height / 35,
+                        width: width,
+                        child: Row(
+                          children: [
+                            SizedBox(
+                              width: width * 0.7,
+                            ),
+                            IconButton(
+                              onPressed: () {
+                                print('Edit pressed');
+                              },
+                              icon: Icon(Icons.edit),
+                            )
+                          ],
+                        ),
+                      ),
+                      CircleAvatar(
+                        radius: height * 0.08,
+                        backgroundImage: AssetImage(user.imagePath),
+                      ),
+                      Center(
+                        child: Column(
+                          children: [
+                            Text(
+                              user.name,
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 25,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            Text(
+                              user.email,
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.normal),
+                            ),
+                            Text(
+                              user.about,
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.normal),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ),
@@ -73,11 +134,78 @@ class _ProfileScreenState extends State<ProfileScreen> {
               Align(
                 alignment: Alignment.center,
                 child: Container(
+                  child: Stack(
+                    children: [
+                      ListView.builder(
+                        itemCount: data.length,
+                        itemBuilder: (context, i) => Column(
+                          children: <Widget>[
+                            ListTile(
+                              leading: Container(
+                                width: width / 6,
+                                height: height / 15,
+                                child: Image(
+                                  image: AssetImage(data[i].imagePath),
+                                ),
+                                decoration: BoxDecoration(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(15)),
+                                ),
+                              ),
+                              title: Text(
+                                data[i].title,
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                              subtitle: Container(
+                                padding: const EdgeInsets.only(top: 5.0),
+                                child: Text(
+                                  formattedDate(data[i].date),
+                                  style: TextStyle(
+                                      color: Colors.grey, fontSize: 15.0),
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      Container(
+                        height: height / 19,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.all(Radius.circular(35)),
+                        ),
+                        child: Column(
+                          children: [
+                            SizedBox(
+                              height: height / 67,
+                            ),
+                            Row(
+                              children: [
+                                SizedBox(
+                                  width: width / 30,
+                                ),
+                                Text(
+                                  'Posts',
+                                  style: TextStyle(
+                                      fontSize: height / 35,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                SizedBox(
+                                  width: width / 20,
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                   width: width * 0.85,
                   height: height * 0.4,
                   decoration: BoxDecoration(
                     color: Colors.white,
-                    borderRadius: BorderRadius.all(Radius.circular(35)),
+                    borderRadius:
+                        BorderRadius.all(Radius.circular(height / 38)),
                   ),
                 ),
               )
