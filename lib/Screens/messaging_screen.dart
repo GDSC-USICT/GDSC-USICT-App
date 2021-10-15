aecd9c44ec944a74f4cea3c72e4b08513b4c85f2
Rahul Gandhi <65856344+RGTechno@users.noreply.github.com>
2021-10-15 15:41:40 +0530

    basic homescreen and connected screens

--- lib/Screens/messaging_screen.dart
+++ lib/Screens/messaging_screen.dart
@@ -6,23 +6,28 @@ class MessagingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        resizeToAvoidBottomInset: true,
-        body: Container(
-          color: Colors.blueAccent,
+      resizeToAvoidBottomInset: true,
+      body: Container(
+        color: Colors.blueAccent,
+        child: SingleChildScrollView(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
               Padding(
-                  padding: EdgeInsets.fromLTRB(10, 30, 20, 10),
-                  child: Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text(
-                        "GDSC USICT Community Forum",
-                        style: textStyle(Colors.white, 25.0, FontWeight.bold),
-                      ))),
+                padding: EdgeInsets.fromLTRB(10, 30, 20, 10),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "GDSC USICT Community Forum",
+                    style: textStyle(Colors.white, 25.0, FontWeight.bold),
+                  ),
+                ),
+              ),
               Messages()
             ],
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
